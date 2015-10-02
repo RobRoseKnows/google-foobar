29bcdc5afee4d932ca74d87f25513977ff215cbe
Robert Rose <bertra97@gmail.com>
2015-10-02 11:00:39 -0400

    Prototype Two
    This works on the testcases I used. I'm going to put it into Google
    Foobar as soon as it stops giving me 500 errors.

--- When It Rains It Pours/Answer.java
+++ When It Rains It Pours/Answer.java
@@ -2,7 +2,6 @@ package com.google.challenges;
 
 /**
  * @author Robert Rose
- * 
  */
 
 public class Answer {
@@ -14,15 +13,15 @@ public class Answer {
 	/**
 	 * Called recursively to fill one level of hutch-tops with water at a
 	 * time. When a hutch-top is filled with water, area is incremented.
-	 * @param heights
-	 * @param area
-	 * @return
+	 * @param heights The different heights of hutches.
+	 * @param area Cumulative area going in
+	 * @return The cumulative area coming out
 	 */
 	public static int recurseAndFillBuckets(int[] heights, int area) {
 		int newArea = area;
 		int[] newHeights = heights.clone();
 		for(int i = 1; i < heights.length - 1; i++) {  // No need to check the first and last for bucketness
-			if(heights[i] < heights[i-1] && heights[i] < heights[i+1]) {
+			if(!checkOpenOnLeft(heights, i) && !checkOpenOnRight(heights, i)) {
 				newArea++;
 				newHeights[i]++;
 			}
@@ -32,6 +31,38 @@ public class Answer {
 			return newArea;
 		else
 			return recurseAndFillBuckets(newHeights, newArea);
+	}
+	
+	/**
+	 * Checks to the left of the current hutch to see if it will run off
+	 * the side.
+	 * @param heights The array of hutch heights
+	 * @param checkFrom The current hutch
+	 * @return True if water will run off, false otherwise.
+	 */
+	public static boolean checkOpenOnLeft(int[] heights, int checkFrom) {
+		boolean status = true;
+		int checkFromHeight = heights[checkFrom];
+		for(int i = 0; i < checkFrom && status; i++) { // The && status gives some added efficency
+			status &= heights[i] <= checkFromHeight;
+		}
+		return status;
+	}
+	
+	/**
+	 * Checks to the right of the current hutch to see if it will run off
+	 * the side.
+	 * @param heights The array of hutch heights
+	 * @param checkFrom The current hutch
+	 * @return True if water will run off, false otherwise.
+	 */
+	public static boolean checkOpenOnRight(int[] heights, int checkFrom) {
+		boolean status = true;
+		int checkFromHeight = heights[checkFrom];
+		for(int i = checkFrom + 1; i < heights.length && status; i++) { // The && status adds some efficency.
+			status &= heights[i] <= checkFromHeight;
+		}
+		return status;
 	}
 	
 	public static void main(String[] args) {
