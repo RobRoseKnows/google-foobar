ad2745818021d61fe0d4ba4b7000422252c77c28
Robert Rose <bertra97@gmail.com>
2015-11-23 21:11:43 -0500

    Done.
    Implemented BigInteger.

--- Binary Bunnies/solution.java
+++ Binary Bunnies/solution.java
@@ -1,4 +1,6 @@
-package com.google.challenges; 
+package com.google.challenges;
+
+import java.math.BigInteger;
 
 /**
  * I tried to solve this multiple ways. I figured I'd be doing
@@ -6,12 +8,27 @@ package com.google.challenges;
  * something with queues that didn't work so I ended up looking
  * up the basic statistics. That's when I remembered binomial
  * coefficients. From there I did the rest recursively.
+ * 
+ * My first try, of course, didn't pass test case 4 & 5.
+ * I now needed to find out why. That was because my fancy linear
+ * time binomial coefficient function wasn't working once a number
+ * got big. It was then I understood why the prompt requested output
+ * as a String. I was expected to handle big numbers.
+ * 
+ * It was then I realized I would have much rather used Python
+ * for this problem.
+ * 
+ * Damn it. I'm going to have to use BigInteger
+ * 
+ * Done?
+ * 
+ * Yes!
  */
 
 public class Answer {   
     public static String answer(int[] seq) { 
 
-    	long combos = 0;
+    	BigInteger combos = BigInteger.ZERO;
     	Answer outer = new Answer();
     	Node tree = outer.new Node(seq[0]);
     	for(int i = 1; i < seq.length; i++) {
@@ -27,12 +44,12 @@ public class Answer {
      * Recursively checks to get the number of permutations in the 
      * tree.
      */
-    public static long permutations(Node node) {
+    public static BigInteger permutations(Node node) {
     	int lessSize = 0;
-    	long lessPerm = 1;
+    	BigInteger lessPerm = BigInteger.ONE;
     	
     	int greaterSize = 0;
-    	long greaterPerm = 1;
+    	BigInteger greaterPerm = BigInteger.ONE;
     	
     	if(node.less != null) {
     		lessSize = treeSize(node.less);
@@ -44,10 +61,10 @@ public class Answer {
     		greaterPerm = permutations(node.greater);
     	}
     	
-    	long nCk = binoCoef(lessSize + greaterSize, greaterSize);
+    	BigInteger nCk = binoCoef(lessSize + greaterSize, greaterSize);
     	
     	// Number of ways you can order subtree nodes X number of permutations under both subtrees
-    	return nCk * lessPerm * greaterPerm;
+    	return nCk.multiply(lessPerm).multiply(greaterPerm);
     }
     
     /* Clever way of doing n choose k in linear time. 
@@ -56,11 +73,11 @@ public class Answer {
      * out if there was something in the Math library
      * to do it. (There's not)
      */
-    public static long binoCoef(int n, int k) {
+    public static BigInteger binoCoef(int n, int k) {
     	if(k == 0)
-    		return 1;
+    		return BigInteger.ONE;
     	else
-    		return binoCoef(n, k - 1) * (n - (k - 1)) / k;
+    		return (binoCoef(n, k - 1).multiply(BigInteger.valueOf((n - (k - 1)))).divide(BigInteger.valueOf(k)));
     }
     
     // This recursively checks the size of the tree.
@@ -76,14 +93,14 @@ public class Answer {
     	return 1 + sizeLess + sizeGreater;
     }
     
-    /* For testing:*/
+    /* For testing:
      public static void main(String[] args) {
     	 int[] seq = {1};
     	 System.out.println(answer(seq));
     	 int[] seq2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     	 System.out.println(answer(seq2));
     	 System.out.println(binoCoef(50, 9));
-    }
+    }*/
     
     private class Node {
     	// The less than leaf of the tree.
