5e4cc213a3f86f8c6376d88d6022bf9c098f7bab
Robert Rose <bertra97@gmail.com>
2015-12-01 17:33:13 -0500

    Binary search
    Yeah. About that.

--- Breeding Like Rabbits/solution.java
+++ Breeding Like Rabbits/solution.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
  * find another way to get it working.
  *
  * I decided to start plugging in numbers into an Excel spreadsheet to see if 
- * I could find a pattern or a formula. 
+ * I could find a pattern or a formula. That didn't work.
  * 
  * I decided to try to do recursion augmented by HashTables in order to speed
  * up calculations. This is functional programming technique. By keeping all the
@@ -30,75 +30,60 @@ import java.util.HashMap;
  * I was also able to run the function with 2^63 in 2ms. And 10^25 in 3ms.
  * 
  * After seeing how fast it can run using functional programming I decided
- * that checking all the answers wasn't such a bad idea after all. I was going to
- * need a better way to access it however, with the number of integers I am
- * working with collisions are a possibility in a HashMap.
- * 
- * I chose to implement a Linked List to store all the solved values as it
- * would allow me to iterate through the list in linear time. I will also be
- * able to add all the entries in linear time by using a pointer.
- * 
- * I also realized that the LinkedList won't initially be sorted due to the
- * nature of how the formulas work. Since it would be much easier to be able to
- * stop the loop once we pass where the function should be, I decided to set up
- * a sorting system to sort the list prior to iterating through. 
- * 
- * I chose to use Merge Sort as the sorting function because it guarantees 
- * O(nlogn) time. Unlike Merge Sort with arrays, Merge Sort with LinkedLists does
- * not require O(N) space (which would've been un desirable as it would've been
- * huge!
+ * that I should just search for the answer through all possible answers.
+ * I then realized that I didn't have to build the entire set of numbers! I just
+ * needed to build the ones that I needed and I can do that by implementing
+ * a modified binary sort.
  */
 
 public class Answer {   
 	
 	// The functional programming HashMap 
 	private static HashMap<BigInteger, BigInteger> hash = new HashMap<BigInteger, BigInteger>();
-	
-	// The root of a LinkedList containing every R(n) up to n = 10^25
-	private static Node root;
 
 	
     public static String answer(String str_S) {
-    	// If the LinkedList hasn't been made yet, make it.
-    	if(root == null){
-    		root = new Node(BigInteger.ZERO, BigInteger.ONE);
-    		
-    		// Use a pointer so that adding is O(1) and in a linear loop.
-    		Node pointer = root; 
-    		
-        	long start = System.currentTimeMillis();
-    		// Since str_S will be limited to numbers 10^25 and less, we can build only the numbers up to (10^24)/2 because R of it is 1.03 * 10^25
-    		for(BigInteger i = BigInteger.ONE; i.compareTo(BigInteger.TEN.pow(24).divide(BigInteger.valueOf(2))) <= 0; i.add(BigInteger.ONE)) {
-    			System.out.println("Call: " + i);
-    			pointer.addNode(i, R(i));
-    			pointer = pointer.next; // Move pointer forward
-    			if(i.mod(BigInteger.TEN).equals(BigInteger.ZERO))
-    				System.out.println(i + " at " + (System.currentTimeMillis() - start));
-    		}
-    		System.out.println("Done building LL");
-    	}
-    	
-    	String answer = "None"; // Default to "None"
     	BigInteger searchFor = new BigInteger(str_S);
-    	
-    	Node on = root;
-    	while(on.next != null) {
-    		// Check to see if we're on the right one. 
-    		if(searchFor.equals(on.val)) {
-    			answer = "" + on.i;
-    			break;
-    		}
-    		
-    		on = on.next;
+    	BigInteger location = binarySearch(searchFor, BigInteger.ZERO, BigInteger.TEN.pow(25));
+    	if(location.equals(BigInteger.valueOf(-1))) {
+    		return "None";
+    	} else {
+    		return location.toString();
     	}
-    	
-    	
-    	return answer;
     } 
     
+    public static BigInteger binarySearch(BigInteger searchFor, BigInteger min, BigInteger max) {    	
+    	BigInteger distance = max.subtract(min);
+    	BigInteger checkSpot = min.add(distance.divide(BigInteger.valueOf(2)));
+    	
+    	BigInteger rabbits = R(checkSpot);
+    	
+    	if(searchFor.equals(rabbits)) { // rabbits == number of rabbits we want
+    		return checkSpot;
+    	} else if(min.equals(max)){ // There is nowhere left to search
+    		return BigInteger.valueOf(-1);
+    	} else if(rabbits.compareTo(searchFor) > 0) { // too many rabbits (r > sF)
+    		return binarySearch(searchFor, min, checkSpot);
+    	} else if(rabbits.compareTo(searchFor) < 0) { // too few rabbits (r < sF)
+    		return binarySearch(searchFor, checkSpot, max);
+    	}
+    	
+    	return BigInteger.valueOf(-1);
+    }
+    
+    /**
+     * This takes a time and computes the number of rabbits at that time.
+     * Augmented by the power of HashTables.
+     * @param x A time to check
+     * @return Number of rabbits alive at x time.
+     */
     public static BigInteger R(BigInteger x) {
     	BigInteger n = x.divide(BigInteger.valueOf(2));
     	//System.out.println(x.toString());
+    	
+    	if(hash.containsKey(x))
+    		return hash.get(x);
+    	
     	if(x.equals(BigInteger.ZERO)) {
     		// R(0) = 1
     		return BigInteger.ONE;
@@ -162,42 +147,8 @@ public class Answer {
     	long end = System.currentTimeMillis();
     	System.out.println(sol + " in " + (end-start) + "ms");
     	*/
+
     	System.out.println("100: " + answer("100"));
     	System.out.println("7: " + answer("7"));
-    }
-    
-    public static class Node implements Comparable<Object> {
-    	private BigInteger val;
-    	private BigInteger i;
-    	private Node next = null;
-    	
-    	/**
-    	 * Constructor
-    	 * @param index The X value for R(X)
-    	 * @param value R(X)
-    	 */
-    	public Node(BigInteger index, BigInteger value) {
-    		//System.out.println("i: " + index + " v: " + value);
-    		val = value;
-    		i = index;
-    	}
-    	
-    	/**
-    	 * This function adds a new Node to the current Node.
-    	 * @param index The X value for R(X) 
-    	 * @param value R(X)
-    	 */
-    	public void addNode(BigInteger index, BigInteger value) {
-    		Node end = new Node(index, value);
-    		this.next = end;
-    	}
-
-		/**
-		 * All that's necessary is to call the compareTo method on the value
-		 * since that's what we're comparing.
-		 */
-		public int compareTo(Object node) {
-			return val.compareTo(((Node) node).val);
-		}
     }
 }
